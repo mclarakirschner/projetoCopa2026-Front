31fefca1327033690c6fce173b043e08a6919095
Maria Clara Kirschner <mariaclarakirschner@gmail.com>
2026-06-09 00:40:46 -0300

    Backend Copa funcionando

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,16 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { TimeComponent } from './time-component/time-component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { provideHttpClient } from '@angular/common/http';
 import { HomeComponent } from './home-component/home-component';
-import { FooterComponent } from './footer-component/footer-component';
-import { NavBarComponent } from './nav-bar-component/nav-bar-component';
-import { RankingComponent } from './ranking-component/ranking-component';
+import { TimeComponent } from './time-component/time-component';
 import { PartidaComponent } from './partida-component/partida-component';
+import { RankingComponent } from './ranking-component/ranking-component';
+import { NavBarComponent } from './nav-bar-component/nav-bar-component';
+import { FooterComponent } from './footer-component/footer-component';
 
 @NgModule({
   declarations: [
@@ -21,10 +21,16 @@ import { PartidaComponent } from './partida-component/partida-component';
     RankingComponent,
     NavBarComponent,
     FooterComponent
-
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient()],
-  bootstrap: [App],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
 })
 export class AppModule { }
